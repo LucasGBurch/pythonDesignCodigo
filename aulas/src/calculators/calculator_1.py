8f194d6a2941e04dc8f653bf1f630c95743052c7
burchlg <88804181+LucasGBurch@users.noreply.github.com>
2024-04-19 08:46:59 -0300

    Introdução aos testes unitários - Primeiro Teste
    Aula introdutória e do primeiro teste unitário.

--- aulas/src/calculators/calculator_1.py
+++ aulas/src/calculators/calculator_1.py
@@ -48,6 +48,6 @@ class Calculator1:
         return {
             "data": {
                 "Calculator": 1,
-                "result": calc_result
+                "result": round(calc_result, 2)
             }
         }
