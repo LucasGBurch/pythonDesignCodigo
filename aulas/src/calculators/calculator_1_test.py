d8ae15c355248127bfac6eff3ffbe81669e75179
burchlg <88804181+LucasGBurch@users.noreply.github.com>
2024-04-19 15:36:41 -0300

    Testando Erros
    Aula de teste de erros

--- aulas/src/calculators/calculator_1_test.py
+++ aulas/src/calculators/calculator_1_test.py
@@ -1,4 +1,5 @@
 from typing import Dict
+from pytest import raises
 from .calculator_1 import Calculator1
 
 
@@ -9,10 +10,11 @@ class MockRequest:
 
 def test_calculate():
     mock_request = MockRequest(body={"number": 1})
-
     calculator_1 = Calculator1()
 
     response = calculator_1.calculate(mock_request)
+    print()
+    print(response)
 
     # Testar o formato da resposta:
     assert "data" in response
@@ -22,3 +24,13 @@ def test_calculate():
     # Testar assertividade da resposta:
     assert response["data"]["result"] == 14.25
     assert response["data"]["Calculator"] == 1
+
+
+def test_calculate_with_body_error():
+    mock_request = MockRequest(body={"something": 1})
+    calculator_1 = Calculator1()
+
+    with raises(Exception) as excinfo:
+        calculator_1.calculate(mock_request)
+
+    assert str(excinfo.value) == "body mal formatado!"
